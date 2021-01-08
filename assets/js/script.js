d8a91fd8891ec366370dd2af430ba1950a64e09c
Kirk <kirkbalop@gmail.com>
2021-01-08 15:05:01 -0800

    added moment js and date edit css

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+//   check due date
+auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -43,6 +45,26 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var auditTask = function(taskEl) {
+    // get date from task element
+    var date = $(taskEl).find("span").text().trim();
+    console.log(date);
+
+    // convert to moment object at 5pm
+    var time = moment(date, "L").set("hour", 17);
+    console.log(time);
+
+    // remove any old classes from element
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+    // apply new class if task is near/over date
+    if (moment().isAfter(time)) {
+        $(taskEl).addClass("list-group-item-danger");
+    }
+    else if (Math.abs(moment().diff(time, "days")) <= 2) {
+        $(taskEl).addClass("list-group-item-warning");
+    }
 };
 
 $(".list-group").on("click", "p", function() {
@@ -92,10 +114,17 @@ $(".list-group").on("click", "span", function(){
 
     $(this).replaceWith(dateInput);
 
+    dateInput.datepicker({
+        minDate: 1,
+        onClose: function(){
+            $(this).trigger("change");
+        }
+    });
+
     dateInput.trigger("focus");
 });
 
-$(".list-group").on("blur", "input[type='text']", function(){
+$(".list-group").on("change", "input[type='text']", function(){
     var date = $(this)
         .val()
         .trim();
@@ -117,6 +146,8 @@ $(".list-group").on("blur", "input[type='text']", function(){
         .text(date);
 
     $(this).replaceWith(taskSpan);
+
+    auditTask($(taskSpan).closest("list-group-item"));
 });
 
 $(".card .list-group").sortable({
@@ -224,4 +255,6 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+$("#modalDueDate").datepicker({
+    minDate: 1
+});
